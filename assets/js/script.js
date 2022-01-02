665f1f2940c718004c574a472b5381b889b037a5
Jorge Molina <jorge.e.molina@hotmail.com>
2022-01-02 10:00:10 -0600

    adding css style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 var apiKey = "05f629a6a4d93216a57d8401030d50a8";
 var today = moment().format("L");
 var searchHistory = [];
+var apiUrl = ""
 
+
+fetch(apiUrl).then(function(respond) {
+    if (respond.ok) {
+        respond.json().then(function(data) {
+            displayIssues(data);
+        })
+    }
+})
+
+for (var i = 0; i <)
