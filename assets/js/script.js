6a66d32ba4e8fc7b951eeb2bf5c17542bc750902
Jorge Molina <jorge.e.molina@hotmail.com>
2022-01-08 13:17:53 -0600

    updating script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,19 @@
-var apiKey = "05f629a6a4d93216a57d8401030d50a8";
-var searchHistory = [];
-var cityName = cityName
-var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + "Houston" + "&appid=" + apiKey;
-
-
-var button = document.querySelector(".button");
-//var inputValue = document.querySelector("");
-var name = document.querySelector(".name");
-
+var apiKey = "05f629a6a4d93216a57d8401030d50a8"
+var userFormEl = document.querySelector("#user-form");
+var cityName = document.querySelector("#city-name");
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+};
+document.getElementById("city-name").addEventListener("submit", formSubmitHandler);
 
 
 fetch(apiUrl).then(function(response) {
-    
-
     if (response.ok) {
         console.log(response);
-        response.json().then (function(data){
+        response.json().then (function(data) {
             console.log(data);
         })
-       
     }
 })
