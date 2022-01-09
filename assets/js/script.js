648fca371250d3bd999e90a168a24736029ad6cb
Jorge Molina <jorge.e.molina@hotmail.com>
2022-01-09 15:44:25 -0600

    updating css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,40 @@
-$(document).ready(function () {
 var apiKey = "05f629a6a4d93216a57d8401030d50a8"
-var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#city-name");
-var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + "Houston" + "&appid=" + apiKey;
+var userFormEl = document.querySelector("#form-input");
+var cityInput = document.querySelector("#city-input");
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + apiKey;
+var city = document.getElementById("city").value;
 
+var formSumbitHandler = function(event){
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } else{
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+}
+
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+function getWeather(event) {
+    event.preventDefault();
+}
+var getWeather = function(city) {
+        
+} 
+
+    fetch(apiUrl)
+    .then(function(response) {
+        response.json().then(function(data) {
+            displayWeather(data, city);
+        })
+    })
+
+    cityInput.addEventListener("submit", formSumbitHandler);
